e2d7a6632a500cecbce988221c9f8d1c96d95c6f
sunjianwei <sunjianwei@zcshow.com>
2017-11-02 00:02:27 +0800

    差值器1

--- app/src/main/java/com/tck/animationstudy/InterpolatorFragment.java
+++ app/src/main/java/com/tck/animationstudy/InterpolatorFragment.java
@@ -1,15 +1,30 @@
 package com.tck.animationstudy;
 
 import android.content.Context;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -29,6 +44,8 @@ public class InterpolatorFragment extends Fragment implements View.OnClickListen
     private Button left;
     private Button right;
 
+    Interpolator[] interpolators = new Interpolator[13];
+    Path interpolatorPath;
 
     public static InterpolatorFragment newInstance(@LayoutRes int sampleLayoutRes) {
         InterpolatorFragment fragment = new InterpolatorFragment();
@@ -45,6 +62,24 @@ public class InterpolatorFragment extends Fragment implements View.OnClickListen
         ViewStub sampleStub = (ViewStub) view.findViewById(R.id.sampleStub);
         sampleStub.setLayoutResource(sampleLayoutRes);
         sampleStub.inflate();
+
+        interpolatorPath = new Path();
+        interpolatorPath.lineTo(0.25f, 0.25f);
+        interpolatorPath.moveTo(0.25f, 1.5f);
+        interpolatorPath.lineTo(1, 1);
+        interpolators[0] = new AccelerateDecelerateInterpolator();
+        interpolators[1] = new LinearInterpolator();
+        interpolators[2] = new AccelerateInterpolator();
+        interpolators[3] = new DecelerateInterpolator();
+        interpolators[4] = new AnticipateInterpolator();
+        interpolators[5] = new OvershootInterpolator();
+        interpolators[6] = new AnticipateOvershootInterpolator();
+        interpolators[7] = new BounceInterpolator();
+        interpolators[8] = new CycleInterpolator(0.5f);
+        interpolators[9] = PathInterpolatorCompat.create(interpolatorPath);
+        interpolators[10] = new FastOutLinearInInterpolator();
+        interpolators[11] = new FastOutSlowInInterpolator();
+        interpolators[12] = new LinearOutSlowInInterpolator();
 
         interpolatorSpinner = (AppCompatSpinner) view.findViewById(R.id.interpolatorSpinner);
         imageView = (ImageView) view.findViewById(R.id.imageView);
@@ -74,12 +109,25 @@ public class InterpolatorFragment extends Fragment implements View.OnClickListen
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.up:
-                imageView.animate().alpha(0);
+                imageView.animate()
+                        .translationX(DpUtils.dpToPixel(150))
+                        .setDuration(600)
+                        .setInterpolator(interpolators[interpolatorSpinner.getSelectedItemPosition()])
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        imageView.setTranslationX(0);
+                                    }
+                                }, 500);
+                            }
+                        });
                 break;
             case R.id.down:
                 imageView.animate().alpha(1);
                 break;
-
         }
     }
 }
